bd34f002a81f75123102cbd9a1664a5c0ff4831a
helen-brooks <helen.brooks@ukaea.uk>
2020-08-10 14:55:01 +0100

    Moved OBB private helper functions outside class and into a namespace so they can be used externally.

--- src/geom/obb.cpp
+++ src/geom/obb.cpp
@@ -1,4 +1,5 @@
 #include "obb.hpp"
+#include <limits>
 
 namespace DAGMC {
 
@@ -6,60 +7,73 @@ void  OrientedBoundingBox::construct_obb(const_element_iterator elemBegin,
                                          const_element_iterator elemEnd) {
 
   // Get all element vertices and put in matrix
-  Matrix points = getPointsMatrix(elemBegin, elemEnd);
+  Matrix points;
+  OBBUtils::getPointsMatrix(elemBegin, elemEnd, points);
 
   // Construct covariance matrix and find eigenbasis
   Matrix basis;
-  constructBasis(points, basis);
+  OBBUtils::constructBasis(points, basis);
 
   // Find the extremal points along the basis vecs
   Vector minPoint;
   Vector maxPoint;
-  findExtremalPoints(points, basis, minPoint, maxPoint);
+  OBBUtils::findExtremalPoints(points, basis, minPoint, maxPoint);
 
   // Construct a box with the min/max point aligned with basis vecs
-  constructBox(basis, minPoint, maxPoint);
+  box = std::make_shared<Box>(maxPoint, minPoint, basis);
 
 }
 
 // Construct a matrix of points
-Matrix OrientedBoundingBox::getPointsMatrix(const_element_iterator elemBegin,
-                                            const_element_iterator elemEnd) {
+void OBBUtils::getPointsMatrix(const_element_iterator elemBegin,
+                               const_element_iterator elemEnd,
+                               Matrix& points) {
+  points.reset();
 
-  // Declare 1X3 Matrix
-  Matrix pointsMatrix(1, 3);
-
-  // Loop over elements
-  for (; elemBegin != elemEnd; ++elemBegin) {
-    const Elem& element = **elemBegin;
-    unsigned int nNodes = element.n_nodes();
-    for (unsigned int iNode = 0; iNode < nNodes; iNode++) {
-      // Get the point associated with this node
-      const Point& point = element.point(iNode);
-      // Create a new row in matrix using initializer list.
-      Matrix pointRow(1, 3) = {point(0), point(1), point(2)};
-      // Insert row
-      pointsMatrix.insert_rows(1, pointRow);
+  libmesh_try {
+    // Loop over elements
+    for (; elemBegin != elemEnd; ++elemBegin) {
+      const libMesh::Elem& element = **elemBegin;
+      unsigned int nNodes = element.n_nodes();
+      for (unsigned int iNode = 0; iNode < nNodes; iNode++) {
+        // Get the point associated with this node
+        const libMesh::Point& point = element.point(iNode);
+        // Create a new row in matrix using initializer list.
+        Matrix pointRow = {point(0), point(1), point(2)};
+        // Insert row
+        if (points.n_rows == 0) {
+          points = pointRow;
+        } else {
+          points.insert_rows(points.n_rows, pointRow);
+        }
+      }
     }
   }
-  return pointsMatrix;
-
+  libmesh_catch(libMesh::LogicError & e) {
+    points.reset();
+  }
 }
 
 
 // Construct a basis from a matrix of points
-void OrientedBoundingBox::constructBasis(Matrix& points, Matrix& basis) {
+void OBBUtils::constructBasis(Matrix& points, Matrix& basis) {
 
   // Calculate the covariance matrix for the points
   Matrix covMat = cov(points);
 
+  // Check the covariance matrix is square and symmetric
   if (covMat.n_rows != DIM || covMat.n_cols != DIM)
+    return;
+  else if (!covMat.is_symmetric())
     return;
 
   // Find the eigenvectors of the covariance matrix
   Vector eigvals;
-  eigs_sym(eigvals, eigvec, covMat, DIM);
-  if (eigvec.n_rows != DIM || eigven.n_cols != DIM)
+  Matrix eigvec;
+  bool solved = arma::eig_sym(eigvals, eigvec, covMat);
+  if (!solved)
+    return;
+  if (eigvec.n_rows != DIM || eigvec.n_cols != DIM)
     return;
 
   // Ensure columns are normalised to unity
@@ -67,81 +81,53 @@ void OrientedBoundingBox::constructBasis(Matrix& points, Matrix& basis) {
 
 }
 
-void OrientedBoundingBox::findExtremalPoints(Matrix& points, Matrix& basis,
-                                             Vector& minPoint, Vector& maxPoint) {
+void OBBUtils::findExtremalPoints(Matrix& points, Matrix& basis,
+                                  Vector& minPoint, Vector& maxPoint) {
+
+  // Initialise min/max points
+  minPoint.zeros(DIM);
+  maxPoint.zeros(DIM);
 
   if (basis.n_rows != DIM || basis.n_cols != DIM)
     return;
   if (points.n_cols != DIM)
     return;
 
-  // Placeholder for min and max coeffs in basis
-  std::array<double, DIM> minCoeffs;
-  std::array<double, DIM> maxCoeffs;
-
   // points has a row for every vertex
   unsigned int nPoints = points.n_rows;
+  if (nPoints == 0)
+    return;
+
+  // Find the coeffs of the min/max points along the basis vectors
+  double MAX_DOUBLE = std::numeric_limits<double>::max();
+  std::vector<double> minCoeffs(DIM, MAX_DOUBLE);
+  std::vector<double> maxCoeffs(DIM, -MAX_DOUBLE);
   for (unsigned int ipoint = 0; ipoint < nPoints; ++ipoint) {
-    RowVec row = points(ipoint);
+    RowVec row = points.row(ipoint);
     // Need to tranpose
-    Vector pvec = row.trans();
+    Vector pvec = row.t();
     // Loop over every basis vector
     for (unsigned int idim = 0; idim < DIM; ++idim) {
-      Vector bvec = basis(idim);
+      Vector bvec = basis.col(idim);
       // Project out coeff along this basis vector
       double coeff = dot(pvec, bvec);
 
-      if (ipoint == 0) {
-        minCoeffs[idim] = coeff;
-        maxCoeffs[idim] = coeff;
-      } else {
-        // Is this the min max coeff so far?
-        if (coeff < minCoeffs[idim]) {
-          minCoeff[idim] = coeff;
-        }
-        if (coeff > maxCoeffs[idim]) {
-          maxCoeff[idim] = coeff;
-        }
+      // Is this the min or max coeff so far?
+      if (coeff < minCoeffs.at(idim)) {
+        minCoeffs.at(idim) = coeff;
       }
+      if (coeff > maxCoeffs.at(idim)) {
+        maxCoeffs.at(idim) = coeff;
+      }
+
     }// End loop over basis
   }// End loop over points
 
-
-  // Now fetch the extremal points wrt the original axies
-  minPoint.zeros();
-  maxPoint.zeros();
+  // Now fetch the extremal points wrt the original axes
   for (unsigned int idim = 0; idim < DIM; ++idim) {
-    minPoint += minCoef[idim] * basis(idim);
-    maxPoint += maxCoef[idim] * basis(idim);
+    minPoint += minCoeffs.at(idim) * basis(idim);
+    maxPoint += maxCoeffs.at(idim) * basis(idim);
   }
-
-
-}
-
-
-void OrientedBoundingBox::constructBox(Matrix& basis, Vector& minPoint, Vector& maxPoint) {
-
-  // Find the vector to get between the extremal points
-  Vector diff = maxPoint - minPoint;
-
-  // Project the diff along the basis to get lengths of sides of box
-  std::array<double, DIM> sides;
-  for (unsigned int idim = 0; idim < DIM; ++idim) {
-    sides[idim] = dot(diff, basis(idim));
-  }
-
-  std::array<Vector, 8> boxPoints;
-  boxPoints[0] = minPoint;
-  boxPoints[1] = boxPoints[0] + sides[0] * basis(0);
-  boxPoints[2] = boxPoints[0] + sides[1] * basis(1);
-  boxPoints[3] = boxPoints[1] + sides[1] * basis(1);
-  boxPoints[4] = boxPoints[0] + sides[2] * basis(2);
-  boxPoints[5] = boxPoints[4] + sides[0] * basis(0);
-  boxPoints[6] = boxPoints[4] + sides[1] * basis(1);
-  boxPoints[7] = boxPoints[5] + sides[1] * basis(1);
-
-  //To-do check boxPoints[7] = maxPoint
-
 }
 
 }
--- src/geom/obb.hpp
+++ src/geom/obb.hpp
@@ -1,41 +1,66 @@
-#include <armadillo>
+#include "box.hpp"
+// Libmesh headers
+#include "libmesh/libmesh.h"
+#include "libmesh/mesh.h"
+#include "libmesh/mesh_base.h"
+#include "libmesh/elem.h"
+#include "libmesh/node.h"
 
 namespace DAGMC {
 
-typedef armadillo::mat Matrix;
-typedef armadillo::vec Vector;
-typedef armadillo::rowvec RowVec;
 const unsigned int DIM = 3;
 
+typedef libMesh::MeshBase::const_element_iterator const_element_iterator;
+
+// Class to construct a box containing a set of libMesh elements
 class OrientedBoundingBox {
 
  public:
   OrientedBoundingBox(const_element_iterator elemBegin,
-                      const_element_iterator elemEnd) {
+                      const_element_iterator elemEnd):
+    elBegin(elemBegin), elEnd(elemBegin) {
     construct_obb(elemBegin, elemEnd);
   }
 
+  bool isConstructed() { return (box != nullptr); }
+  bool isSane() { return (box == nullptr) ? false : box->isSane(); };
+  int  status() { return (box == nullptr) ? Box::failunknown : box->getBoxStatus(); };
+
+  // Return a const reference to box
+  const Box& getBox() { return *box; };
 
  private:
   // Construct an OBB for an element set
   void construct_obb(const_element_iterator elemBegin,
                      const_element_iterator elemEnd);
 
-  // Construct a matrix of points
-  Matrix getpointsMatrix(const_element_iterator elemBegin,
-                         const_element_iterator elemEnd);
+  // The actual box
+  std::shared_ptr<Box> box;
 
-  // Construct a basis from a matrix of points
-  void constructBasis(Matrix& points, Matrix& basis);
+  // Iterators to elements contained inside the box
+  const_element_iterator elBegin;
+  const_element_iterator elEnd;
 
-  // Given a set of points and a basis, find the most extreme coefficients
-  // along each basis vector
-  void findExtremalPoints(Matrix& points, Matrix& basis,
-                          Vector& minPoint, Vector& maxPoint);
+  // A list of element IDs contained in the box?
 
-  // Given a basis, and a min/max point, construct a box
-  void constructBox(Matrix& basis, Vector& minPoint, Vector& maxPoint);
+};
 
-}
+// Some  helper functions
+// Keep this separate so they can be used externally in principle
+namespace OBBUtils {
+// Construct a matrix of points
+void getPointsMatrix(const_element_iterator elemBegin,
+                     const_element_iterator elemEnd,
+                     Matrix& points);
+
+// Construct a basis from a matrix of points
+void constructBasis(Matrix& points, Matrix& basis);
+
+// Given a set of points and a basis, find the most extreme coefficients
+// along each basis vector
+void findExtremalPoints(Matrix& points, Matrix& basis,
+                        Vector& minPoint, Vector& maxPoint);
+
+};
 
 }
