15fa4c004a3b2b6ef764af4f2b4784818ccd4ca5
helen-brooks <helen.brooks@ukaea.uk>
2020-08-11 10:06:05 +0100

    Added alternative methdods for constructing OBB. Compiles, but as yet untested.

--- src/geom/obb.cpp
+++ src/geom/obb.cpp
@@ -6,13 +6,18 @@ namespace DAGMC {
 void  OrientedBoundingBox::construct_obb(const_element_iterator elemBegin,
                                          const_element_iterator elemEnd) {
 
-  // Get all element vertices and put in matrix
-  Matrix points;
-  OBBUtils::getPointsMatrix(elemBegin, elemEnd, points);
-
-  // Construct covariance matrix and find eigenbasis
+  // Find basis vectors for the box
   Matrix basis;
-  OBBUtils::constructBasis(points, basis);
+  Matrix points;
+  if (method == ConstructMethod::CONT) {
+    OBBUtils::constructBasisCont(elemBegin, elemEnd, basis, points);
+  } else if (method == ConstructMethod::DISCRETE) {
+    OBBUtils::constructBasisDiscrete(elemBegin, elemEnd, basis, points);
+  } else {
+    //Just get points and an axis-aligned basis
+    OBBUtils::getPointsMatrix(elemBegin, elemEnd, points);
+    basis.eye(DIM, DIM);
+  }
 
   // Find the extremal points along the basis vecs
   Vector minPoint;
@@ -24,7 +29,42 @@ void  OrientedBoundingBox::construct_obb(const_element_iterator elemBegin,
 
 }
 
-// Construct a matrix of points
+void OBBUtils::constructBasisCont(const_element_iterator elemBegin,
+                                  const_element_iterator elemEnd,
+                                  Matrix& basis, Matrix& points) {
+
+  // Compute statistics on this element set
+  std::vector<double> areas;
+  Vector mean;
+  OBBUtils::getElemStats(elemBegin, elemEnd, areas, mean, points);
+
+  // Construct the covariance matrix from the statistics
+  Matrix cov;
+  OBBUtils::calcCov(areas, mean, points, cov);
+
+  // Compute basis
+  OBBUtils::constructBasisFromCov(cov, basis);
+
+}
+
+
+// Construct basis using the covariance of the nodes of elements
+void OBBUtils::constructBasisDiscrete(const_element_iterator elemBegin,
+                                      const_element_iterator elemEnd,
+                                      Matrix& basis, Matrix& points) {
+
+  // Get all element vertices and put in matrix
+  OBBUtils::getPointsMatrix(elemBegin, elemEnd, points);
+
+  // Construct the covariance matrix for the points
+  // Pass transpose: function expects entries are rows
+  Matrix covMat = arma::cov(points.t());
+
+  // Compute basis
+  OBBUtils::constructBasisFromCov(covMat, basis);
+
+}
+// Construct a matrix of points from the nodes of a set of elements
 void OBBUtils::getPointsMatrix(const_element_iterator elemBegin,
                                const_element_iterator elemEnd,
                                Matrix& points) {
@@ -39,12 +79,12 @@ void OBBUtils::getPointsMatrix(const_element_iterator elemBegin,
         // Get the point associated with this node
         const libMesh::Point& point = element.point(iNode);
         // Create a new row in matrix using initializer list.
-        Matrix pointRow = {point(0), point(1), point(2)};
-        // Insert row
-        if (points.n_rows == 0) {
-          points = pointRow;
+        Vector pvec = {point(0), point(1), point(2)};
+        // Insert column
+        if (points.n_cols == 0) {
+          points = pvec;
         } else {
-          points.insert_rows(points.n_rows, pointRow);
+          points.insert_cols(points.n_cols, pvec);
         }
       }
     }
@@ -54,23 +94,83 @@ void OBBUtils::getPointsMatrix(const_element_iterator elemBegin,
   }
 }
 
+void OBBUtils::getElemStats(const_element_iterator elemBegin,
+                            const_element_iterator elemEnd,
+                            std::vector<double>& areas, Vector& mean,
+                            Matrix& points) {
+  points.reset();
+  mean.reset();
+  areas.clear();
 
-// Construct a basis from a matrix of points
-void OBBUtils::constructBasis(Matrix& points, Matrix& basis) {
+  libmesh_try {
 
-  // Calculate the covariance matrix for the points
-  Matrix covMat = cov(points);
+    // Loop over elements
+    for (; elemBegin != elemEnd; ++elemBegin) {
+      const libMesh::Elem& element = **elemBegin;
+      unsigned int nNodes = element.n_nodes();
 
+      // Require elments are tris
+      libMesh::ElemType type = element.type();
+      if (type != libMesh::ElemType::TRI3 || nNodes != 3) {
+        points.reset();
+        return;
+      }
+
+      // Fetch coords of this element
+      std::vector< Vector > elemPoints;
+      for (unsigned int iNode = 0; iNode < nNodes; iNode++) {
+        // Get the point associated with this node
+        const libMesh::Point& point = element.point(iNode);
+        // Get point
+        Vector pvec = {point(0), point(1), point(2)};
+        // Insert column
+        if (points.n_cols == 0) {
+          points = pvec;
+        } else {
+          points.insert_cols(points.n_cols, pvec);
+        }
+        elemPoints.push_back(pvec);
+      }
+
+      const Vector& p = elemPoints.at(0);
+      const Vector& q = elemPoints.at(1);
+      const Vector& r = elemPoints.at(2);
+
+      // Compute area
+      double area = 0.5 * arma::norm(arma::cross(q - p, r - p));
+      areas.push_back(area);
+
+      // Compute contribution to mean
+      mean += (p + q + r) / (area * 6.0);
+
+    } // End loop over elems
+
+    // Fetch number of elems
+    unsigned int nElems = areas.size();
+
+    // Correctly normalise mean vector
+    mean *= (1.0 / double(nElems));
+
+  }
+  libmesh_catch(libMesh::LogicError & e) {
+    points.reset();
+    mean.reset();
+    areas.clear();
+  }
+
+}
+
+void OBBUtils::constructBasisFromCov(Matrix& cov, Matrix& basis) {
   // Check the covariance matrix is square and symmetric
-  if (covMat.n_rows != DIM || covMat.n_cols != DIM)
+  if (cov.n_rows != DIM || cov.n_cols != DIM)
     return;
-  else if (!covMat.is_symmetric())
+  else if (!cov.is_symmetric())
     return;
 
   // Find the eigenvectors of the covariance matrix
   Vector eigvals;
   Matrix eigvec;
-  bool solved = arma::eig_sym(eigvals, eigvec, covMat);
+  bool solved = arma::eig_sym(eigvals, eigvec, cov);
   if (!solved)
     return;
   if (eigvec.n_rows != DIM || eigvec.n_cols != DIM)
@@ -78,8 +178,48 @@ void OBBUtils::constructBasis(Matrix& points, Matrix& basis) {
 
   // Ensure columns are normalised to unity
   basis = normalise(eigvec);
-
 }
+
+void OBBUtils::calcCov(std::vector<double>& areas, Vector& mean,
+                       Matrix& points, Matrix& cov) {
+
+  cov.reset();
+
+  // Check dimensions are self-consistent
+  unsigned int nElems = areas.size();
+  if (mean.n_rows != DIM)
+    return;
+  if (points.n_rows != DIM || points.n_cols != 3 * nElems)
+    return;
+
+  // Initialise cov matrix
+  cov.zeros(DIM, DIM);
+
+  for (unsigned int iElem = 0; iElem < nElems; iElem++) {
+
+    // Fetch elem points and subtract mean
+    unsigned int iPoint = 3 * iElem;
+    Vector pBar = points.col(iPoint) - mean;
+    Vector qBar = points.col(iPoint + 1) - mean;
+    Vector rBar = points.col(iPoint + 2) - mean;
+    Vector sum = pBar + qBar + rBar;
+
+    // Compute outer products
+    Matrix outerSum = arma::kron(sum, sum.t());
+    outerSum       += arma::kron(pBar, pBar.t());
+    outerSum       += arma::kron(qBar, qBar.t());
+    outerSum       += arma::kron(rBar, rBar.t());
+
+    // Normalise
+    outerSum *= 1.0 / (24.0 * double(nElems) * areas.at(iElem));
+
+    // Add contribution to covariance
+    cov +=  outerSum;
+
+  }
+  //Done
+}
+
 
 void OBBUtils::findExtremalPoints(Matrix& points, Matrix& basis,
                                   Vector& minPoint, Vector& maxPoint) {
@@ -90,11 +230,11 @@ void OBBUtils::findExtremalPoints(Matrix& points, Matrix& basis,
 
   if (basis.n_rows != DIM || basis.n_cols != DIM)
     return;
-  if (points.n_cols != DIM)
+  if (points.n_rows != DIM)
     return;
 
-  // points has a row for every vertex
-  unsigned int nPoints = points.n_rows;
+  // points has a col for every vertex
+  unsigned int nPoints = points.n_cols;
   if (nPoints == 0)
     return;
 
@@ -103,9 +243,7 @@ void OBBUtils::findExtremalPoints(Matrix& points, Matrix& basis,
   std::vector<double> minCoeffs(DIM, MAX_DOUBLE);
   std::vector<double> maxCoeffs(DIM, -MAX_DOUBLE);
   for (unsigned int ipoint = 0; ipoint < nPoints; ++ipoint) {
-    RowVec row = points.row(ipoint);
-    // Need to tranpose
-    Vector pvec = row.t();
+    Vector pvec = points.col(ipoint);
     // Loop over every basis vector
     for (unsigned int idim = 0; idim < DIM; ++idim) {
       Vector bvec = basis.col(idim);
--- src/geom/obb.hpp
+++ src/geom/obb.hpp
@@ -8,16 +8,22 @@
 
 namespace DAGMC {
 
+// 3D space is assumed throughout
 const unsigned int DIM = 3;
+
+enum ConstructMethod { CONT = 0, DISCRETE, ALIGNED };
 
 typedef libMesh::MeshBase::const_element_iterator const_element_iterator;
 
 // Class to construct a box containing a set of libMesh elements
+// Default is oriented, but can build aligned box
 class OrientedBoundingBox {
 
  public:
   OrientedBoundingBox(const_element_iterator elemBegin,
-                      const_element_iterator elemEnd):
+                      const_element_iterator elemEnd,
+                      ConstructMethod methodIn = ConstructMethod::CONT) :
+    method(methodIn),
     elBegin(elemBegin), elEnd(elemBegin) {
     construct_obb(elemBegin, elemEnd);
   }
@@ -34,6 +40,8 @@ class OrientedBoundingBox {
   void construct_obb(const_element_iterator elemBegin,
                      const_element_iterator elemEnd);
 
+  const ConstructMethod method;
+
   // The actual box
   std::shared_ptr<Box> box;
 
@@ -41,20 +49,61 @@ class OrientedBoundingBox {
   const_element_iterator elBegin;
   const_element_iterator elEnd;
 
-  // A list of element IDs contained in the box?
+};
+
+//  Functionally a typedef, just restricts the constructor
+class AxisAlignedBoundingBox: public OrientedBoundingBox {
+
+ public:
+  AxisAlignedBoundingBox(const_element_iterator elemBegin,
+                         const_element_iterator elemEnd) :
+    OrientedBoundingBox(elemBegin, elemEnd, ConstructMethod::ALIGNED) {};
+
+  ~AxisAlignedBoundingBox() {};
 
 };
 
 // Some  helper functions
 // Keep this separate so they can be used externally in principle
 namespace OBBUtils {
+
+// Construct basis using the covariance of the nodes of elements.
+// This is the first method presented in "OBBTree:
+// A Hierarchical Structure for Rapid Interference Detection",
+// Gottschalk, Lin, and Manocha
+// section 4
+void constructBasisDiscrete(const_element_iterator elemBegin,
+                            const_element_iterator elemEnd,
+                            Matrix& basis, Matrix& points);
+
+// Construct basis using the covariance with a continuous average
+// over each element (assumes tris).
+// This is the second method presented in "OBBTree:
+// A Hierarchical Structure for Rapid Interference Detection",
+// Gottschalk, Lin, and Manocha
+// section 4
+void constructBasisCont(const_element_iterator elemBegin,
+                        const_element_iterator elemEnd,
+                        Matrix& basis, Matrix& points);
+
 // Construct a matrix of points
 void getPointsMatrix(const_element_iterator elemBegin,
                      const_element_iterator elemEnd,
                      Matrix& points);
 
-// Construct a basis from a matrix of points
-void constructBasis(Matrix& points, Matrix& basis);
+// Compute all statistics needed to calculate covariance from a
+// set of elems. Assumes tris.
+void getElemStats(const_element_iterator elemBegin,
+                  const_element_iterator elemEnd,
+                  std::vector<double>& areas, Vector& mean,
+                  Matrix& points);
+
+// Compute the covariance given various statistics
+void calcCov(std::vector<double>& areas, Vector& mean,
+             Matrix& points, Matrix& cov);
+
+// Construct a basis from a covariance matrix
+void constructBasisFromCov(Matrix& cov, Matrix& basis);
 
 // Given a set of points and a basis, find the most extreme coefficients
 // along each basis vector
