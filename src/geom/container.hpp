4ee318c3b0e19de17f1eb25e6328f7251639c96f
helen-brooks <helen.brooks@ukaea.uk>
2020-08-14 11:19:51 +0100

    Added a convenience class to contain a set of objects, and in particular libmesh elements. To be used in OrientedBoundingBox, to allow for different storage methods of elements, since libmesh iterators are a bit clunky.

---
+++ src/geom/container.hpp
@@ -0,0 +1,120 @@
+#include<set>
+
+// Libmesh headers
+#include "libmesh/libmesh.h"
+#include "libmesh/mesh.h"
+#include "libmesh/mesh_base.h"
+#include "libmesh/elem.h"
+#include "libmesh/node.h"
+
+namespace DAGMC {
+
+typedef libMesh::MeshBase::const_element_iterator const_element_iterator;
+
+// An abstract iterable set of type T objects
+template < class T >
+class Container {
+
+ public:
+  Container() {};
+  ~Container() {};
+
+  // Send internal iterator to start
+  virtual void reset() = 0;
+
+  // Set input pointer to next in list, incrementing internal iterator
+  virtual bool getNext(T*& refptr) = 0;
+
+  // Fetch copies of first and last in list
+  virtual T* first() = 0;
+  virtual T* last() = 0;
+
+};
+
+// A container for libMesh elements
+typedef Container<const libMesh::Elem> ElemContainer;
+
+// A container for libMesh elements  constructed from iterators
+class ElemConstItContainer : public ElemContainer {
+
+ public:
+  ElemConstItContainer(const_element_iterator elemBegin,
+                       const_element_iterator elemEnd) :
+    elBegin(elemBegin),
+    elEnd(elemEnd),
+    elIt(elemEnd),
+    it_is_void(true) {};
+
+  ~ElemConstItContainer() {};
+
+  void reset() override {
+    it_is_void = true;
+  }
+
+  const libMesh::Elem* first() override { return &** elBegin; };
+  const libMesh::Elem* last() override { return &** elEnd; };
+
+  bool getNext(const libMesh::Elem*& next) override {
+    if (it_is_void) {
+      elIt = elBegin;
+      it_is_void = false;
+    } else
+      elIt++;
+    if (elIt != elEnd) {
+      next = &** elIt;
+      return true;
+    } else
+      return false;
+  }
+
+ private:
+
+  const_element_iterator elBegin;
+  const_element_iterator elEnd;
+  const_element_iterator elIt;
+  bool it_is_void;
+
+};
+
+// A container for libMesh elements  constructed from a set of pointers
+class ElemConstPtrContainer : public ElemContainer {
+
+ public:
+  ElemConstPtrContainer(std::set< const libMesh::Elem*> elemsIn) :
+    elems(elemsIn) {
+    reset();
+  };
+
+  void reset() override {
+    it_is_void = true;
+  }
+
+  const libMesh::Elem* first() override { return *elems.begin(); };
+  const libMesh::Elem* last() override { return *elems.end(); };
+
+  bool getNext(const libMesh::Elem*& next) override {
+    if (it_is_void) {
+      it = elems.begin();
+      it_is_void = false;
+    } else
+      it++;
+    if (it != elems.end()) {
+      next = *it;
+      return true;
+    } else
+      return false;
+  }
+
+
+ private:
+
+  // Set of constant pointers to contant elements
+  const std::set< const libMesh::Elem*> elems;
+
+  // Iterator
+  std::set< const libMesh::Elem*>::const_iterator it;
+  bool it_is_void;
+
+};
+
+}
---
+++ src/geom/tests/container_test.cpp
@@ -0,0 +1,152 @@
+#include <gtest/gtest.h>
+#include "container.hpp"
+
+// Super-lightweight test for the container class.
+
+//---------------------------------------------------------------------------//
+// TEST FIXTURES
+//---------------------------------------------------------------------------//
+
+class ContainerTest : public ::testing::Test {
+
+ protected:
+
+  ContainerTest() : libMeshException(false),
+    meshLoaded(false),
+    filename("cube.e") {};
+
+  virtual void SetUp() override {
+    initlibMesh();
+  };
+  virtual void TearDown() override {};
+
+  void initlibMesh() {
+    //Emulate dummy command line args since required by libmesh
+    int argc_dummy = 1;
+    char dummych[] = "dummy";
+    char* argv_dummy[] = { dummych };
+
+    // Initialize libMesh and handle any exceptions
+    libmesh_try {
+      // Initialize the library
+      initPtr = std::make_shared<libMesh::LibMeshInit>(argc_dummy, argv_dummy);
+      // Create the mesh
+      if (initPtr != nullptr) {
+        meshPtr = std::make_shared<libMesh::Mesh>(initPtr->comm());
+      }
+      // Read in the mesh data
+      if (meshPtr != nullptr) {
+        // Clear any prior data
+        meshPtr->clear();
+        meshPtr->read(filename);
+        meshLoaded = meshPtr->is_prepared();
+      }
+    }
+    libmesh_catch(libMesh::LogicError & e) {
+      libMeshException = true;
+      return;
+    }
+  }
+
+  // Data members required for tests
+  // Pointers to libMesh objects
+  std::shared_ptr<libMesh::LibMeshInit> initPtr;
+  std::shared_ptr<libMesh::Mesh> meshPtr;
+  // Save whether libMesh raised an exception
+  bool libMeshException;
+  bool meshLoaded;
+  std::string filename;
+  std::shared_ptr<DAGMC::ElemContainer> dagElems;
+
+};
+
+
+//---------------------------------------------------------------------------//
+// FIXTURE BASED TESTS
+//---------------------------------------------------------------------------//
+
+TEST_F(ContainerTest, Constructor) {
+
+  ASSERT_FALSE(libMeshException);
+  ASSERT_NE(initPtr, nullptr);
+  ASSERT_NE(meshPtr, nullptr);
+  ASSERT_TRUE(meshLoaded);
+
+  // Get element iterators
+  DAGMC::const_element_iterator elBeg
+    = meshPtr->elements_begin();
+  DAGMC::const_element_iterator elEnd
+    = meshPtr->elements_end();
+  DAGMC::const_element_iterator elIt = elBeg;
+
+  std::set<const libMesh::Elem*> elemptrs;
+  std::vector<const libMesh::Elem*> elemptrsOrd;
+  unsigned int nElems = meshPtr->n_elem();
+
+  // Normal loop to set pointers for comparison
+  for (; elIt != elEnd; ++elIt) {
+    const libMesh::Elem& elem = **elIt;
+    elemptrsOrd.push_back(&elem);
+    elemptrs.insert(&elem);
+  }
+  // Need to check that setup didn't fail
+  ASSERT_EQ(elemptrs.size(), nElems);
+  ASSERT_EQ(elemptrsOrd.size(), nElems);
+
+  // First constructor call
+  dagElems =
+      std::make_shared<DAGMC::ElemConstItContainer>(elBeg, elEnd);
+
+  EXPECT_EQ(dagElems->first(), &** elBeg);
+  EXPECT_EQ(dagElems->last(), &** elEnd);
+
+  //Two attempts to check reset call works
+  unsigned int attempts = 0;
+  std::stringstream err;
+  do {
+    attempts++;
+    err.str("");
+    err << "Failed on attempt " << attempts;
+
+    const libMesh::Elem* elemPtr;
+    unsigned int count = 0;
+    while (dagElems->getNext(elemPtr)) {
+      ASSERT_LT(count, nElems) << err.str();
+      // Expect to hit the same elements in same order
+      EXPECT_EQ(elemPtr, elemptrsOrd.at(count)) << err.str();
+      count++;
+    }
+    EXPECT_EQ(count, nElems) << err.str();
+
+    dagElems->reset();
+
+  } while (attempts < 2);
+
+  // Second constructor call
+  dagElems =
+      std::make_shared<DAGMC::ElemConstPtrContainer>(elemptrs);
+
+  EXPECT_EQ(dagElems->first(), *elemptrs.begin());
+  EXPECT_EQ(dagElems->last(), *elemptrs.end());
+
+  //Two attempts to check reset call works
+  attempts = 0;
+  do {
+    attempts++;
+    err.str("");
+    err << "Failed on attempt " << attempts;
+
+    const libMesh::Elem* elemPtr;
+    auto itCheck = elemptrs.begin();
+    while (dagElems->getNext(elemPtr)) {
+      ASSERT_NE(itCheck, elemptrs.end());
+      EXPECT_EQ(elemPtr, *itCheck);
+      itCheck++;
+    }
+
+    //check reset call works
+    dagElems->reset();
+
+  } while (attempts < 2);
+
+}
